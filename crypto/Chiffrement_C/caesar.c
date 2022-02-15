094ae88202e4e6fc9bbe83ba456decd01e74390b
XMR-D <88503168+XMR-D@users.noreply.github.com>
2022-02-15 17:29:17 +0100

    Minors Bugs fixing

--- crypto/Chiffrement_C/caesar.c
+++ crypto/Chiffrement_C/caesar.c
@@ -4,46 +4,9 @@
 #include <SFML/Graphics.h>
 #include <SFML/Window.h>
 #include <errno.h>
+#include "Tools.h"
 
 #define IMG_PATH "./Tools/caesar.bmp"
-
-
-void caesar2(char msg[], int key)
-{
-    for(size_t i = 0; msg[i] != '\0'; i++)
-    {
-        if(msg[i] <= 90 && msg[i] >= 65)
-                msg[i] = ((msg[i]-90)+key) % 26 + 90;
-        else
-        {
-            if(msg[i] <= 122 && msg[i] >= 97)
-                 msg[i] = ((msg[i]-122)+key) % 26 + 122;
-        }
-    }
-}
-
-sfText * SetText(char str[], int x, int y, sfFont *f, sfColor col, int size)
-{
-
-
-    sfVector2f coord = {x,y};
-    sfText *string = sfText_create();
-    sfText_setString(string, str);
-    sfText_setPosition(string, coord);
-    sfText_setCharacterSize(string, size);
-    sfText_setFont(string, f);
-    sfText_setColor(string, col);
-    return string;
-}
-
-char* c_to_s(char c)
-{
-    char *res = malloc(3 * sizeof(char));
-    res[0] = c;
-    res[1] = ' ';
-    res[2] = '\0';
-    return res;
-}
 
 
 int main(int argc, char * argv[])
